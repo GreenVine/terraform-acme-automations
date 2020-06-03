c23dfa4f43dce6102be22ccacc90435538146ef9
Robin Liu <opensource@greenvine.dev>
2020-06-03 19:58:40 +0800

    feat(cert-archiver): add initial implementation of Certificate Archiver

---
+++ modules/cert-archiver/main.tf
@@ -0,0 +1,25 @@
+locals {
+  archiving_objects = {
+    "cert.key"         = var.cert_private_key
+    "leaf.crt"         = var.cert_leaf
+    "intermediate.crt" = var.cert_intermediate
+    "fullchain.p12"    = var.cert_fullchain_p12
+    "fullchain.pem"    = var.cert_fullchain_pem
+  }
+}
+
+resource "google_storage_bucket_object" "objects_archive" {
+  for_each = local.archiving_objects
+
+  bucket       = var.storage_bucket
+  name         = "${trim(var.storage_prefix, "/\\")}/${var.cert_request_id}/${each.key}"
+  content      = each.value
+  content_type = "text/plain; charset=utf-8"
+  metadata     = {
+    "x-acme-account-id"  = var.acme_account_fingerprint
+    "x-acme-cert-id"     = var.cert_provider_id
+    "x-acme-request-id"  = var.cert_request_id
+    "x-cert-common-name" = var.cert_cn
+    "x-cert-sans"        = join(",", var.cert_sans)
+  }
+}
---
+++ modules/cert-archiver/variables.tf
@@ -0,0 +1,66 @@
+// Mandatory variables
+variable "storage_bucket" {
+  description = "The Google Cloud Storage bucket name for certificate storage"
+  type        = string
+}
+
+variable "cert_request_id" {
+  description = "Certificate request identifier"
+  type        = string
+}
+
+variable "cert_provider_id" {
+  description = "Certificate identifier within the ACME CA"
+  type        = string
+}
+
+variable "cert_private_key" {
+  description = "Certificate private key"
+  type        = string
+}
+
+variable "cert_leaf" {
+  description = "Leaf certificate"
+  type        = string
+}
+
+variable "cert_intermediate" {
+  description = "Intermediate certificate"
+  type        = string
+}
+
+variable "cert_fullchain_pem" {
+  description = "Certificate full chain in PEM format"
+  type        = string
+}
+
+variable "cert_fullchain_p12" {
+  description = "Certificate full chain and private key in PKCS12 format"
+  type        = string
+}
+
+// Optional variables
+variable "storage_prefix" {
+  description = "The object prefix for all archives"
+  type        = string
+  default     = ""
+}
+
+variable "acme_account_fingerprint" {
+  description = "ACME account fingerprint"
+  type        = string
+  default     = ""
+}
+
+// Non-essential variables (for tracking purpose only)
+variable "cert_cn" {
+  description = "Certificate common name"
+  type        = string
+  default     = ""
+}
+
+variable "cert_sans" {
+  description = "Certificate subject alternative names (SANs)"
+  type        = list(string)
+  default     = []
+}
